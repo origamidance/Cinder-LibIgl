5e8d5ceef6e3f6bd986034fa78efc775ad711c8f
origamidance <origamidance@gmail.com>
2018-12-07 18:47:02 +0100

    updated cinderlibigl.h/.cpp

---
+++ include/CinderLibIgl_backup.h
@@ -0,0 +1,75 @@
+/**
+ *
+ *
+ */
+
+#pragma once
+
+#include "cinder/gl/VboMesh.h"
+#include "cinder/TriMesh.h"
+#include "cinder/Color.h"
+#include <igl/igl_inline.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/per_face_normals.h>
+#include <igl/per_corner_normals.h>
+#include <igl/AABB.h>
+
+namespace cinder {
+typedef std::shared_ptr<class IglMesh> IglMeshRef;
+class IglMesh : public TriMesh {
+  using TriMesh::TriMesh;
+ public:
+  IglMesh();
+  IglMesh(const std::string str);
+  IglMesh(Eigen::MatrixXd V, Eigen::MatrixXi F, Eigen::MatrixXd C=Eigen::MatrixXd()) {
+    IglMesh();
+    setMesh(V, F, C);
+  };
+  virtual ~IglMesh();
+  bool loadMesh(const std::string str);
+  void setMesh(Eigen::MatrixXd V, Eigen::MatrixXi F,Eigen::MatrixXd C=Eigen::MatrixXd());
+  void setColor(Eigen::MatrixXd C);
+  igl::AABB<Eigen::MatrixXd, 3> getAABBTree() { return aabbTree; };
+
+ public:
+  cinder::gl::VboMeshRef mVboMeshRef;
+  std::vector<gl::VboMesh::Layout> bufferLayout;
+
+  Eigen::MatrixXd mTV;
+  Eigen::MatrixXi mTT;
+  Eigen::MatrixXi mTF;
+  Eigen::MatrixXd mV;
+  Eigen::MatrixXd mC;
+  Eigen::MatrixXi mF;
+  Eigen::MatrixXi mE;
+
+ private:
+  igl::AABB<Eigen::MatrixXd, 3> aabbTree;
+
+  TriMesh::Format fmt;
+
+  Eigen::MatrixXd N_vertices;
+  Eigen::MatrixXd N_faces;
+  Eigen::MatrixXd N_corners;
+  /* Eigen::MatrixXf V_vbo; */
+  /* Eigen::MatrixXf V_normals_vbo; */
+  /* Eigen::MatrixXf V_ambient_vbo; */
+  /* Eigen::MatrixXf V_diffuse_vbo; */
+  /* Eigen::MatrixXf V_specular_vbo; */
+  /* Eigen::MatrixXf V_uv_vbo; */
+  /* Eigen::MatrixXf lines_V_vbo; */
+  /* Eigen::MatrixXf lines_V_colors_vbo; */
+  /* Eigen::MatrixXf points_V_vbo; */
+  /* Eigen::MatrixXf points_V_colors_vbo; */
+
+  int tex_u;
+  int tex_v;
+  Eigen::Matrix<char, Eigen::Dynamic, 1> tex;
+
+  /* Eigen::Matrix<unsigned, Eigen::Dynamic, Eigen::Dynamic> F_vbo; */
+  /* Eigen::Matrix<unsigned, Eigen::Dynamic, Eigen::Dynamic> lines_F_vbo; */
+  /* Eigen::Matrix<unsigned, Eigen::Dynamic, Eigen::Dynamic> points_F_vbo; */
+
+  /* Eigen::MatrixXf C_vbo; */
+};
+}
--- src/CinderLibIgl_backup.cpp
+++ src/CinderLibIgl_backup.cpp
@@ -1,11 +1,8 @@
 #include "CinderLibIgl.h"
 #include "cinder/app/App.h"
-#include "cinder/Log.h"
-#include "cinder/gl/gl.h"
 #include <igl/read_triangle_mesh.h>
 #include <igl/barycenter.h>
 #include <igl/edges.h>
-
 
 using namespace std;
 using namespace ci;
@@ -13,23 +10,21 @@ using namespace ci::app;
 
 namespace cinder {
     IglMesh::IglMesh() {
+      fmt = TriMesh::Format().positions().normals();
+      initFromFormat(fmt);
       bufferLayout = {
               gl::VboMesh::Layout().usage(GL_DYNAMIC_DRAW).attrib(geom::Attrib::POSITION, 3),
-              gl::VboMesh::Layout().usage(GL_DYNAMIC_DRAW).attrib(geom::Attrib::NORMAL, 3),
               gl::VboMesh::Layout().usage(GL_DYNAMIC_DRAW).attrib(geom::Attrib::COLOR, 3)
       };
-      auto lambert = gl::ShaderDef().lambert().color();
-      mShaderRef= gl::getStockShader( lambert );
     }
 
     IglMesh::IglMesh(const std::string str) {
+      fmt = TriMesh::Format().positions().normals();
+      initFromFormat(fmt);
       bufferLayout = {
               gl::VboMesh::Layout().usage(GL_DYNAMIC_DRAW).attrib(geom::Attrib::POSITION, 3),
-              gl::VboMesh::Layout().usage(GL_DYNAMIC_DRAW).attrib(geom::Attrib::NORMAL, 3),
               gl::VboMesh::Layout().usage(GL_DYNAMIC_DRAW).attrib(geom::Attrib::COLOR, 3)
       };
-      auto lambert = gl::ShaderDef().lambert().color();
-      mShaderRef= gl::getStockShader( lambert );
       Eigen::MatrixXd V;
       Eigen::MatrixXi F;
       if (igl::read_triangle_mesh(str, V, F)) {
@@ -55,22 +50,28 @@ namespace cinder {
       }
     }
 
-  void IglMesh::setShader(gl::GlslProgRef shader){
-      mShaderRef=shader;
-    mBatchRef=gl::Batch::create(mVboMeshRef,mShaderRef);
-    }
-
 
     void IglMesh::setColor(Eigen::MatrixXd C) {
+      if (C.size() == 0) {
+        fmt = TriMesh::Format().positions().normals();
+        initFromFormat(fmt);
+      } else {
+        fmt = TriMesh::Format().positions().normals().colors();
+        initFromFormat(fmt);
+      }
       Eigen::MatrixXf C_vbo = C.transpose().cast<float>();
-      mVboMeshRef->bufferAttrib(geom::Attrib::COLOR, C_vbo.size() * sizeof(float), (float *) C_vbo.data());
+      this->mColors = std::vector<float>(C_vbo.data(), C_vbo.data() + C_vbo.size());
     };
 
     void IglMesh::setMesh(Eigen::MatrixXd V, Eigen::MatrixXi F, Eigen::MatrixXd C) {
+      this->clear();
       mV = V;
       mF = F;
       mC = C;
       igl::edges(mF, mE);
+      // cout << "edges="<<mE << "\n";
+
+      aabbTree.init(mV, mF);
       // Compute per-face normals
       igl::per_face_normals(mV, mF, N_faces);
 
@@ -79,6 +80,9 @@ namespace cinder {
 
       // Compute per-corner normals, |dihedral angle| > 20 degrees --> crease
       igl::per_corner_normals(mV, mF, 20, N_corners);
+      // cout << "N_vertices="<<N_vertices << "\n";
+      // cout << "N_faces="<<N_faces<< "\n";
+      // cout << "N_corners="<<N_corners<< "\n";
       Eigen::MatrixXf V_vbo = V.transpose().cast<float>();
       Eigen::Matrix<unsigned, Eigen::Dynamic, Eigen::Dynamic> F_vbo = F.transpose().cast<unsigned>();
       Eigen::MatrixXf C_vbo;
@@ -86,16 +90,28 @@ namespace cinder {
         C_vbo = Eigen::MatrixXf::Ones(V_vbo.rows(), V_vbo.cols());
       else
         C_vbo = C.transpose().cast<float>();
+      // Eigen::MatrixXf C_vbo= Eigen::MatrixXf::Zero(V.cols(),V.rows())+Eigen::MatrixXf::Ones(V.cols(),V.rows());
+      // Eigen::MatrixXf C_vbo= Eigen::MatrixXf::Random(V.cols(),V.rows())+Eigen::MatrixXf::Ones(V.cols(),V.rows());
       Eigen::MatrixXf V_normals_vbo = N_vertices.transpose().cast<float>();
+      // V_normals_vbo= N_faces.transpose().cast<float>();
+      // V_normals_vbo = N_corners.transpose().cast<float>()
+      this->mPositions = std::vector<float>(V_vbo.data(), V_vbo.data() + V_vbo.size());
+      this->mIndices = std::vector<uint32_t>(F_vbo.data(), F_vbo.data() + F_vbo.size());
+
+      this->mNormals.clear();
+      // (glm::vec3*)V_normals_vbo,V_normals_vbo.data()+V_normals_vbo.size()
+      // this->mNormals=std::vector<vec3>(V_normals_vbo.data(),V_normals_vbo.data()+V_normals_vbo.size());
+      // this->mColors=std::vector<float>(C_vbo.data(),C_vbo.data()+C_vbo.size());
+      this->appendNormals((glm::vec3 *) Eigen::MatrixXf(N_vertices.transpose().cast<float>()).data(),
+                          N_vertices.rows());
+      // this->appendColors((Color*)(Eigen::MatrixXf(mC.transpose().cast<float>())).data(),mC.rows());
+      // mVboMeshRef=gl::VboMesh::create(*this,bufferLayout);
 
       mVboMeshRef = gl::VboMesh::create(V.rows(), GL_TRIANGLES, bufferLayout, F.size(), GL_UNSIGNED_INT);
       mVboMeshRef->bufferAttrib(geom::Attrib::POSITION, V_vbo.size() * sizeof(float), (float *) V_vbo.data());
-      mVboMeshRef->bufferAttrib(geom::Attrib::NORMAL,V_normals_vbo.size()* sizeof(float),(float*) V_normals_vbo.data());
       mVboMeshRef->bufferAttrib(geom::Attrib::COLOR, C_vbo.size() * sizeof(float), (float *) C_vbo.data());
       mVboMeshRef->bufferIndices(F_vbo.size() * sizeof(uint), (uint *) (F_vbo.data()));
-      mBatchRef=gl::Batch::create(mVboMeshRef,mShaderRef);
-    }
-    void IglMesh::draw() {
-      mBatchRef->draw();
+      // std::cout<<"\nVertices=\n"<<V;
+      // std::cout<<"\nFaces=\n"<<F;
     }
 }
